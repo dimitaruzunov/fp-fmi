6c5077bb73387617bea4f006d4bd1011a9b3c288
Dimitar Uzunov <dimitar.uzunov.dev@gmail.com>
2015-10-15 22:56:49 +0300

    find product of digits in number

---
+++ product-digits.scm
@@ -0,0 +1,10 @@
+(define (product-digits n)
+  (define (cut-last-digit n)
+    (remainder n 10))
+
+  (define (cut-number-from-last-digit n)
+    (quotient n 10))
+
+  (if (= n 0)
+      1
+      (* (cut-last-digit n) (product-digits (cut-number-from-last-digit n)))))
