e95be5d3810bc8b2f171cf3891be9fda3e5fb43d
Dimitar Uzunov <dimitar.uzunov.dev@gmail.com>
2015-10-17 12:18:57 +0300

    prime number predicate

---
+++ prime-number.scm
@@ -0,0 +1,9 @@
+(define (prime? n)
+  (define square-root-of-n (sqrt n))
+
+  (define (prime-iter divisor)
+    (cond ((> divisor square-root-of-n) #t)
+          ((= (remainder n divisor) 0) #f)
+          (else (prime-iter (+ divisor 1)))))
+
+  (prime-iter 2))
