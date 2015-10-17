e80ad4978223251c601d3c7d186405563bf8b1ca
Dimitar Uzunov <dimitar.uzunov.dev@gmail.com>
2015-10-17 14:44:47 +0300

    added a tail recursion version

--- week1/product-of-digits.scm
+++ week1/product-of-digits.scm
@@ -5,7 +5,15 @@
   (define (cut-number-from-last-digit n)
     (quotient n 10))
 
-  (if (= n 0)
-      1
-      (* (cut-last-digit n)
-         (product-digits (cut-number-from-last-digit n)))))
+  ;(if (= n 0)
+  ;    1
+  ;    (* (cut-last-digit n)
+  ;       (product-digits (cut-number-from-last-digit n))))
+
+  ; Tail recursion version
+  (define (product-digits-iter n result)
+    (if (= n 0)
+        result
+        (product-digits-iter (cut-number-from-last-digit n)
+                             (* result (cut-last-digit n)))))
+  (product-digits-iter n 1))
