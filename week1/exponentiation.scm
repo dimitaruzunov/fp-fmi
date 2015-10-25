1a110b6d8359a0a1db29acaf0d9e6d6c80370e61
Dimitar Uzunov <dimitar.uzunov.dev@gmail.com>
2015-10-25 13:48:26 +0200

    using unary operator minus instead of (* n -1)

--- week1/exponentiation.scm
+++ week1/exponentiation.scm
@@ -1,5 +1,5 @@
 (define (pow b n)
-  ;(cond ((< n 0) (pow (/ 1 b) (* n -1)))
+  ;(cond ((< n 0) (pow (/ 1 b) (- n)))
   ;      ((= n 0) 1)
   ;      (else (* b (pow b (- n 1)))))
 
@@ -8,7 +8,7 @@
 
   ; Quick pow
   ;(define (qpow b n)
-  ;  (cond ((< n 0) (qpow (/ 1 b) (* n -1)))
+  ;  (cond ((< n 0) (qpow (/ 1 b) (- n)))
   ;        ((= n 0) 1)
   ;        ((even? n) (qpow (square b) (/ n 2)))
   ;        (else (* b (qpow (square b) (quotient n 2))))))
@@ -16,7 +16,7 @@
 
   ; Quick pow tail recursion version
   (define (qpow-iter b n result)
-    (cond ((< n 0) (qpow-iter (/ 1 b) (* n -1) result))
+    (cond ((< n 0) (qpow-iter (/ 1 b) (- n) result))
           ((= n 0) result)
           ((even? n) (qpow-iter (square b) (/ n 2) result))
           (else (qpow-iter (square b) (quotient n 2) (* result b)))))
