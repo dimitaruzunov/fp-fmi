4aeb4653b62c2c239adb9ade781d0a6bb9aee33d
Dimitar Uzunov <dimitar.uzunov.dev@gmail.com>
2015-10-25 13:44:36 +0200

    removed unnecessary conditions

--- week1/exponentiation.scm
+++ week1/exponentiation.scm
@@ -10,16 +10,14 @@
   ;(define (qpow b n)
   ;  (cond ((< n 0) (qpow (/ 1 b) (* n -1)))
   ;        ((= n 0) 1)
-  ;        ((= n 1) b)
   ;        ((even? n) (qpow (square b) (/ n 2)))
-  ;        (else (* b (qpow (square b) (/ (- n 1) 2))))))
+  ;        (else (* b (qpow (square b) (quotient n 2))))))
   ;(qpow b n)
 
   ; Quick pow tail recursion version
   (define (qpow-iter b n result)
     (cond ((< n 0) (qpow-iter (/ 1 b) (* n -1) result))
           ((= n 0) result)
-          ((= n 1) (* result b))
           ((even? n) (qpow-iter (square b) (/ n 2) result))
-          (else (qpow-iter (square b) (/ (- n 1) 2) (* result b)))))
+          (else (qpow-iter (square b) (quotient n 2) (* result b)))))
   (qpow-iter b n 1))
