963dd45dc1a6c68a1eca4e7f5469332977cd2520
Dimitar Uzunov <dimitar.uzunov.dev@gmail.com>
2015-11-08 16:09:29 +0200

    added sum, product and exists? functions

--- higher-order-functions/accumulate.scm
+++ higher-order-functions/accumulate.scm
@@ -28,3 +28,14 @@
                                              (combiner result (term i))
                                              result))))
   (filter-accumulate-iter a null-value))
+
+(define (sum term a next b)
+  (accumulate + 0 term a next b))
+
+(define (product term a next b)
+  (accumulate * 1 term a next b))
+
+(define (exists? term a next b rel? x)
+  (cond ((> a b) #f)
+        ((rel? (term a) x) #t)
+        (else (exists? term (next a) next b rel x))))
