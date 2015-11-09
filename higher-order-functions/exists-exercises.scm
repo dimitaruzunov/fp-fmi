1974ed6c68986fed195f8e29f3b1ce010ba0f62d
Dimitar Uzunov <dimitar.uzunov.dev@gmail.com>
2015-11-09 10:05:52 +0200

    added functions using exists predicate

---
+++ higher-order-functions/exists-exercises.scm
@@ -0,0 +1,35 @@
+(load "exists.scm")
+(load "../week1/factorial.scm")
+
+; finds if element of i^4 + 3in^2 + n^4, i = 1, 2, ..., n such that
+; element divides x exists
+(define (exists1? n x)
+  (define (divisible? t x)
+    (zero? (remainder t x)))
+
+  (define (term i) (+ (* i i i i) (* 3 i n n) (* n n n n)))
+
+  (define (1+ i) (+ i 1))
+
+  (exists? term 1 1+ n divisible? x))
+
+; finds if element of i^4 + 3in^2 + n^4, i = 1, 2, ..., n such that
+; element equals x exists
+(define (exists2? n x)
+  (define (term i) (+ (* i i i i) (* 3 i n n) (* n n n n)))
+
+  (define (1+ i) (+ i 1))
+
+  (exists? term 1 1+ n = x))
+
+; finds if element of x^i / i!, i = 1, 2, ..., n such that
+; element is in [a, b] exists
+(define (exists3? n x a b)
+  (define (in-range? t b)
+    (and (>= t a) (<= t b)))
+
+  (define (term i) (/ (expt x i) (factorial i)))
+
+  (define (1+ i) (+ i 1))
+
+  (exists? term 1 1+ n in-range? b))
