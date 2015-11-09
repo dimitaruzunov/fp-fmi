8e7672e9246e545f6766613970b5e46cc3a37167
Dimitar Uzunov <dimitar.uzunov.dev@gmail.com>
2015-11-09 10:05:11 +0200

    fixed a typo: rel -> rel?

--- higher-order-functions/exists.scm
+++ higher-order-functions/exists.scm
@@ -1,4 +1,4 @@
 (define (exists? term a next b rel? x)
   (cond ((> a b) #f)
         ((rel? (term a) x) #t)
-        (else (exists? term (next a) next b rel x))))
+        (else (exists? term (next a) next b rel? x))))
